9f869dd8550629e91df8aa8b785c11519961dcdb
lizhiliang <lizl@fsmeeting.com>
2017-06-11 00:18:44 +0800

    add server watch

--- ts-gulp/gulpfile.js
+++ ts-gulp/gulpfile.js
@@ -4,7 +4,11 @@ var source = require('vinyl-source-stream');
 var tsify = require('tsify');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject("tsconfig.json")
+var watchify = require('watchify');
+var gutil = require('gulp-util');
 var path = require('path');
+var browsersync = require('browser-sync').create();
+var reload = browsersync.reload;
 
 var path = {
     pages: ['src/*.html']
@@ -15,19 +19,38 @@ gulp.task("copy-html", function() {
         .pipe(gulp.dest('dist'))
 })
 
-gulp.task("default", ["copy-html"], function() {
-        return browserify({
-                basedir: '.',
-                debug: true,
-                entries: ['src/main.ts'],
-                cache: {},
-                packageCache: {}
-            })
-            .plugin(tsify)
-            .bundle()
-            .pipe(source('bundle.js'))
-            .pipe(gulp.dest('dist'))
-    })
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
+
+
+
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest("dist"))
+}
+
+gulp.task("budu", function() {
+    return bundle()
+})
+
+
+gulp.task("default", ["copy-html", "budu"], function() {
+        browsersync.init({
+            port: 3000,
+            server: {
+                baseDir: ['dist']
+            }
+        })
+    }),
+    watchedBrowserify.on("update", bundle)
+watchedBrowserify.on("log", gutil.log)
     // gulp.task("default", function() {
     //     return tsProject.src()
     //         .pipe(tsProject())
